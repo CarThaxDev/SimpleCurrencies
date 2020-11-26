df746b504b259b8a3b5cd0c63e4f00da2b698ac0
CarThax08 <carson.thackston@gmail.com>
2020-11-26 15:02:45 -0500

    Finished the main command. Starting bug testing soon.

--- src/main/java/com/github/carthax08/simplecurrencies/commands/MainCommand.java
+++ src/main/java/com/github/carthax08/simplecurrencies/commands/MainCommand.java
@@ -5,7 +5,11 @@ import com.github.carthax08.simplecurrencies.enums.CommandType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+
+import java.util.Objects;
 
 public class MainCommand implements CommandExecutor {
 
@@ -22,24 +26,99 @@ public class MainCommand implements CommandExecutor {
                 player.sendMessage("You do not have the required permissions to use this command!");
                 return true;
             }else{
-                if(checkCommandType(args[0]) == CommandType.ADD){
-                    if(checkArgsLength(args) == 1){
-                        player.sendMessage("Please provide a currency, player, and amount");
-                        return false;
-                    }else if(checkArgsLength(args) == 2){
-                        player.sendMessage("Please provide a player and amount!");
-                        return false;
-                    }else if(checkArgsLength(args) == 3){
-                        player.sendMessage("Please provide an amount!");
-                        return false;
-                    }
-                }
-                return true;
+                return handleCommand(player, args);
             }
         }else{
             return true;
         }
     }
+
+    public boolean handleCommand(Player player, String[] args){
+        if(checkCommandType(args[0]) == CommandType.ADD){
+            if(checkArgsLength(args) == 1){
+                player.sendMessage("Please provide a currency, player, and amount");
+                return false;
+            }else if(checkArgsLength(args) == 2){
+                player.sendMessage("Please provide a player and amount!");
+                return false;
+            }else if(checkArgsLength(args) == 3){
+                player.sendMessage("Please provide an amount!");
+                return false;
+            }else{
+                YamlConfiguration config = plugin.playerConfigMap.get(player);
+                if(checkCurrencyType(args[1])){
+                    config.set(args[1], config.getInt(args[1]) + Integer.parseInt(args[3]));
+                }else{
+                    player.sendMessage("Unable to perform request. Either the currency doesn't exist or it isn't enabled. Please check the config!");
+                    return false;
+                }
+            }
+        }
+        if(checkCommandType(args[0]) == CommandType.SET){
+            if(checkArgsLength(args) == 1){
+                player.sendMessage("Please provide a currency, player, and amount");
+                return false;
+            }else if(checkArgsLength(args) == 2){
+                player.sendMessage("Please provide a player and amount!");
+                return false;
+            }else if(checkArgsLength(args) == 3){
+                player.sendMessage("Please provide an amount!");
+                return false;
+            }else{
+                YamlConfiguration config = plugin.playerConfigMap.get(player);
+                if(checkCurrencyType(args[1])){
+                    config.set(args[1], Integer.parseInt(args[3]));
+                }else{
+                    player.sendMessage("Unable to perform request. Either the currency doesn't exist or it isn't enabled. Please check the config!");
+                    return false;
+                }
+            }
+        }
+        if(checkCommandType(args[0]) == CommandType.REMOVE){
+            if(checkArgsLength(args) == 1){
+                player.sendMessage("Please provide a currency, player, and amount");
+                return false;
+            }else if(checkArgsLength(args) == 2){
+                player.sendMessage("Please provide a player and amount!");
+                return false;
+            }else if(checkArgsLength(args) == 3){
+                player.sendMessage("Please provide an amount!");
+                return false;
+            }else{
+                YamlConfiguration config = plugin.playerConfigMap.get(player);
+                if(checkCurrencyType(args[1])){
+                    config.set(args[1], config.getInt(args[1]) - Integer.parseInt(args[3]));
+                }else{
+                    player.sendMessage("Unable to perform request. Either the currency doesn't exist or it isn't enabled. Please check the config!");
+                    return false;
+                }
+            }
+        }
+        if(checkCommandType(args[0]) == CommandType.CLEAR){
+            if(checkArgsLength(args) == 1){
+                player.sendMessage("Please provide a currency and a player");
+                return false;
+            }else if(checkArgsLength(args) == 2){
+                player.sendMessage("Please provide a player!");
+                return false;
+            }else{
+                YamlConfiguration config = plugin.playerConfigMap.get(player);
+                if(checkCurrencyType(args[1])){
+                    config.set(args[1], 0);
+                }else{
+                    player.sendMessage("Unable to perform request. Either the currency doesn't exist or it isn't enabled. Please check the config!");
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public boolean checkCurrencyType(String stringToCheck) {
+        return plugin.getConfig().getBoolean("currencies." + stringToCheck + "enabled");
+    }
+
     public CommandType checkCommandType(String stringToCheck){
         if(stringToCheck.equalsIgnoreCase("add")){
             return CommandType.ADD;
