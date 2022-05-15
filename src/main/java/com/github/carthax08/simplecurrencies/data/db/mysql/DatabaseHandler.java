96e82f8f6864e5830d1bd30ae93369dfa755ba9b
CarThax <carson.thackston@gmail.com>
2022-05-14 21:23:16 -0400

    Pushing the changes I have to github.

--- src/main/java/com/github/carthax08/simplecurrencies/data/db/mysql/DatabaseHandler.java
+++ src/main/java/com/github/carthax08/simplecurrencies/data/db/mysql/DatabaseHandler.java
@@ -1,11 +1,18 @@
 package com.github.carthax08.simplecurrencies.data.db.mysql;
 
+import com.github.carthax08.simplecurrencies.data.PluginPlayer;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.bukkit.entity.Player;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class DatabaseHandler {
 
     public static boolean yaml = true;
+    public static HikariDataSource dataSource;
 
     public static HikariDataSource connect(String url, String username, String password){
         yaml = false;
@@ -14,7 +21,25 @@ public class DatabaseHandler {
         config.setUsername(username);
         config.setPassword(password);
 
-        return new HikariDataSource(config);
+        dataSource = new HikariDataSource(config);
+        try(Connection con = dataSource.getConnection()){
+            con.prepareStatement("CREATE DATABSE [IF NOT EXISTS] SC");
+            con.prepareStatement("use SC").execute();
+            boolean tableExists = con.prepareStatement("SHOW FULL TABLES WHERE Table_Name = 'Players'").execute();
+            PreparedStatement pst = con.prepareStatement("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = 'Players'");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return dataSource;
     }
+
+    public PluginPlayer getPlayerFromDB(Player player, String tableName){
+        try(Connection con = dataSource.getConnection()) {
+            PreparedStatement pst = con.prepareStatement("SELECT ALL FROM ")
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
